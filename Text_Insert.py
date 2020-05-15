47a847995fcee5582646c764173852ab4f532e6f
Raj-Srikar <65415209+Raj-Srikar@users.noreply.github.com>
2020-05-15 23:21:23 +0530

    Update Text_Insert.py

--- Text_Insert.py
+++ Text_Insert.py
@@ -6,7 +6,7 @@ def fdur(clip,frno):                #Function takes the video and the frame numb
     return dur
 
 video = VideoFileClip("sample.mp4")
-txt = TextClip("Mov",fontsize=15,color='black')
+txt = TextClip("Mov",fontsize=15,color='black')                                 #Creating a text video file saying "Mov"
 txt = txt.set_position((230,20)).set_duration(0.04)
 vid = CompositeVideoClip([video , txt.set_start(fdur(video,71))])               #Overlay the text clip onto the video
 vid.write_videofile("ESample.mp4")                                              #Create the video file, "ESaample.mp4"
