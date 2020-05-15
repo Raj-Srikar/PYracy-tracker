ad6aee47bee90916b12c90f75d07818661a78fff
Raj-Srikar <65415209+Raj-Srikar@users.noreply.github.com>
2020-05-15 23:10:22 +0530

    Update Text_Insert.py

--- Text_Insert.py
+++ Text_Insert.py
@@ -1,12 +1,12 @@
 from moviepy.editor import *
 
-def fdur(clip,frno):
-    Fps=clip.fps
-    dur = frno/Fps
+def fdur(clip,frno):                #Function takes the video and the frame number as arguments
+    Fps=clip.fps                          #This function returns the duration at which,
+    dur = frno/Fps                              #the frame given is present
     return dur
 
 video = VideoFileClip("sample.mp4")
 txt = TextClip("Mov",fontsize=15,color='black')
 txt = txt.set_position((230,20)).set_duration(0.04)
-vid = CompositeVideoClip([video , txt.set_start(fdur(video,71))])
-vid.write_videofile("ESample.mp4")
+vid = CompositeVideoClip([video , txt.set_start(fdur(video,71))])               #Overlay the text clip onto the video
+vid.write_videofile("ESample.mp4")                                       #Create the video file, "ESaample.mp4"
